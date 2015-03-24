d899d53e498aba51186a75508a5d0d195690a30c
JonahMania <jmania999@gmail.com>
2015-03-23 21:34:45 -0400

    Tests for collisions

--- testHashmap.cpp
+++ testHashmap.cpp
@@ -17,7 +17,15 @@ int main()
     hMap.insert( (char*)"/source/images/test.png", (char*)"testValueSource" );
     std::cout<<"/models/textures/knight.obj: ";
     hMap.insert( (char*)"/models/textures/knight.obj", (char*)"testValueModels" );
-
+	std::cout<<"a: ";
+	hMap.insert( (char*)"a", (char*)"testValueA" );
+	std::cout<<"banana: ";	
+	hMap.insert( (char*)"banana", (char*)"testValueBannaTwo");
+	std::cout<<"a: ";
+	hMap.insert( (char*)"a", (char*)"testValueATwo" );
+	
 	std::cout<<"Find"<<std::endl;
-	std::cout<<hMap.get((char*)"banana")<<std::endl;
+	std::cout<<"banana: "<<hMap.get((char*)"banana")<<std::endl;
+	std::cout<<"/models/textures/knight.obj: "<<hMap.get((char*)"/models/textures/knight.obj")<<std::endl;
+	std::cout<<"a: "<<hMap.get((char*)"a")<<std::endl;
 }
